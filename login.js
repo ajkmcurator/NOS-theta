61d001eb2b7b9177e612f2c0cfbfdf8e93d8cb8e
ajkmcurator <37943014+ajkmcurator@users.noreply.github.com>
2018-04-14 16:34:41 -0600

    Update login.js

--- login.js
+++ login.js
@@ -1,20 +1,16 @@
-// Dependencies
 var socket = io();
-
-// Variables
 var usr = document.getElementById("username");
 var pwd = document.getElementById("password");
 var username = '';
 var password = '';
 
-// Functions
 hashCode = function(str){
     var hash = 0;
     if (str.length == 0) return hash;
     for (i = 0; i < str.length; i++) {
         char = str.charCodeAt(i);
         hash = ((hash<<5)-hash)+char;
-        hash = hash & hash; // Convert to 32bit integer
+        hash = hash & hash;
     }
     return hash;
 }
@@ -38,18 +34,15 @@ function getCookie(cname) {
 function login() {
   username = usr.value;
   password = hashCode(pwd.value);
-  console.log("test");
   socket.emit('auth', [username, password]);
 }
 
-// Upon page load:
 if (getCookie("user") != "" && getCookie("user") !== undefined) {
   username = getCookie("user");
   password = getCookie("key");
   socket.emit('auth', [getCookie("user"), parseInt(getCookie("key"))]);
 }
 
-// Callbacks
 socket.on('err', function(data){
   document.getElementById("errors").innerHTML = data;
   console.log('err : '+data);
